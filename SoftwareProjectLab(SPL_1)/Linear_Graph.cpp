ede8563605e4bfc915ae07db1480498305ee6c08
AmitRoy01 <113287304+AmitRoy01@users.noreply.github.com>
2023-05-25 00:07:16 +0600

    Add files via upload

--- SoftwareProjectLab(SPL_1)/Linear_Graph.cpp
+++ SoftwareProjectLab(SPL_1)/Linear_Graph.cpp
@@ -10,34 +10,51 @@ void Linear_Graph()
     initgraph(&gd, &gm, "");
 
     // Define the linear equation coefficients
-    float a, b,c;
-    //std::cout << "Enter the coefficients m and c of the linear equation (y = mx + c): ";
-//    std::cin >> m >> c;
+    float a1, b1, c1, a2, b2, c2;
+    int lineStyle = 3; // Set the line style to thick
 
     int maxX = getmaxx();
     int maxY = getmaxy();
     int midX = maxX / 2;
     int midY = maxY / 2;
 
-    for(int i=0;i<2;i++){
-    cout << "Enter the coefficients a,b & c of  "<<i+1<<" th linear equation (ax + by = c): ";
-    cin >> a >> b>>c;
+    cout << "Enter the coefficients a, b, and c of the first linear equation (ax + by = c): ";
+    cin >> a1 >> b1 >> c1;
+    cout << "Enter the coefficients a, b, and c of the second linear equation (ax + by = c): ";
+    cin >> a2 >> b2 >> c2;
 
     // Draw the x and y axes
     line(0, midY, maxX, midY);
     line(midX, 0, midX, maxY);
 
-    float m = (-0.25*a/(0.25*b));
-    float t =10*(c/b);
-    // Plot the linear equation graph
-    for (int x = -midX; x < midX; ++x)
-    {
-        int y = (m * x + t);
-        putpixel(midX + x, midY - y, YELLOW);
+    setlinestyle(lineStyle, 0, 1); // Set the line style to thick
+
+    float m1 = (-(0.5*a1) /(0.5* b1));
+    float t1 = 10*(c1 / b1);
+    float m2 = (-(0.5*a2) /(0.5* b2));
+    float t2 = 10*(c2 / b2);
+
+    // Plot the linear equation graphs
+    for (int x = -midX; x < midX; ++x) {
+        int y1 = (m1 * x + t1);
+        int y2 = (m2 * x + t2);
+        putpixel(midX + x, midY - y1, YELLOW);
+        putpixel(midX + x, midY - y2, GREEN);
         delay(0.5);
     }
-    }
-    delay(10000);  // Wait for 10 seconds before closing the window
+
+    setlinestyle(0, 0, 1); // Reset the line style to default
+
+    // Calculate the intersection point
+    float x = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1);
+    float y = (a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1);
+
+    // Print the intersection point
+    char resultStr[100];
+    sprintf(resultStr, "Intersection Point: (x = %.2f, y = %.2f)", x, y);
+    outtextxy(midX + 10, midY + 10, resultStr);
+
+    delay(100000);  // Wait for 10 seconds before closing the window
     closegraph();
     getch();
 }
